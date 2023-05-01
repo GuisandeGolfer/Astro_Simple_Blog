c959e7328447f3d562a9b4572e185e88f524ad26
GuisandeGolfer <DGuisande873@gmail.com>
2023-05-01 16:59:50 -0700

    chore(index.astro): update title and description of SEO

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,7 +1,7 @@
 export const settings = {
-  site: 'https://simple-blog.littlesticks.dev',
-  name: 'Simple Blog',
-  title: 'Site title',
-  description: 'Site description',
+  site: "https://simple-blog.littlesticks.dev",
+  name: "DG's Blog",
+  title: "Site title",
+  description: "Site description",
   showLittleSticksPlug: true,
-}
+};
